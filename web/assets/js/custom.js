5412254dda7df666bd1464b60f7dea0bfdc5a6d6
rajesh <rajesha@onemoney.in>
2020-07-21 00:12:59 +0530

    Jquery form submit added

--- web/assets/js/custom.js
+++ web/assets/js/custom.js
@@ -1,16 +1,24 @@
-$('document').ready(function(){
-let menuList=[];
-const current_path = window.location.pathname.split('/').pop();
-console.log(`current file name from URL is :: ${current_path}`);
-$('.menu_ul > li').each(function(i, obj) {
-    //test
-    let dataUrl=$(this).attr('data-url');
-    menuList.push(dataUrl)
-    if(dataUrl==current_path){
-        $(this).addClass("btn_active");
-    }
-});
-console.log(`menuList loaded...`, menuList);
+$('document').ready(function () {
+    //Menu 
+    activeMenu();
 
-$()
+    //checkingAvailability
+    checkingAvailability();
 });
+function activeMenu() {
+    const current_path = window.location.pathname.split('/').pop();
+    console.log(`current file name from URL is :: ${current_path}`);
+    $('.menu_ul > li').each(function (i, obj) {
+        //test
+        let dataUrl = $(this).attr('data-url');
+        if (dataUrl == current_path) {
+            $(this).addClass("btn_active");
+        }
+    });
+}
+function checkingAvailability() {
+    $('#checkingAvailabilityForm').submit(function(e) {
+        e.preventDefault();
+        console.log($(this).serializeArray());
+    })
+}
